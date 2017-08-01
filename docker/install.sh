afd162b692681f4797000bc9b55b5d1782497316
Pavel Langhammer <pavel.langhammer@gooddata.com>
2017-08-01 20:44:40 +0200

    fix9

--- docker/install.sh
+++ docker/install.sh
@@ -5,4 +5,5 @@ set -x
 USERID=${USERID:-1000}
 GROUPID=${GROUPID:-1000}
 groupadd -fg "$GROUPID" uiuser
-useradd -o uiuser -u "$USERID" -g "$GROUPID"
+useradd -o uiuser -u "$USERID" -g "$GROUPID" -m
+/bin/bash "@"
