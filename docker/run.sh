cd0b0acded359fa3388db06f38823e5b20ec8b5e
Pavel Langhammer <pavel.langhammer@gooddata.com>
2017-08-01 15:03:03 +0200

    fix

--- docker/run.sh
+++ docker/run.sh
@@ -5,12 +5,12 @@ ls -la
 pwd
 #whoami
 #id
-echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > .npmrc
-cat .npmrc
+echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+cat ~/.npmrc
 ssh-agent
 ssh-add -l
 ssh -T git@github.com
-
+yarn cache dir
 echo $HOME
 #echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" >> /code/.npmrc \
 yarn install --pure-lockfile
