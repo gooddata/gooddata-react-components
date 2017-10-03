9f09e3fdfef81035d0a907a547374848565baaa4
vaclavbohac <bohac.v@gmail.com>
2017-10-03 17:11:37 +0200

    fixup! RELATED: RAIL-308 Upgrade TSLint

--- src/components/uri/tests/Visualization.spec.tsx
+++ src/components/uri/tests/Visualization.spec.tsx
@@ -56,12 +56,9 @@ describe('Visualization', () => {
             />
         );
 
-        postpone(
-            () => {
-                expect(wrapper.find(BaseChart).length).toBe(1);
-            },
-            done()
-        );
+        postpone(() => {
+            expect(wrapper.find(BaseChart).length).toBe(1);
+        }, done);
     });
 
     it('should render table', (done) => {
@@ -72,12 +69,9 @@ describe('Visualization', () => {
             />
         );
 
-        postpone(
-            () => {
-                expect(wrapper.find(Table).length).toBe(1);
-            },
-            done()
-        );
+        postpone(() => {
+            expect(wrapper.find(Table).length).toBe(1);
+        }, done);
     });
 
     it('should trigger error in case of given uri is not valid', (done) => {
@@ -114,14 +108,11 @@ describe('Visualization', () => {
             />
         );
 
-        postpone(
-            () => {
-                const node: any = wrapper.getNode();
-                expect(node.dataSource.afm.filters).toHaveLength(1);
-                expect(node.dataSource.afm.filters[0]).toEqual(visFilters[0]);
-            },
-            done()
-        );
+        postpone(() => {
+            const node: any = wrapper.getNode();
+            expect(node.dataSource.afm.filters).toHaveLength(1);
+            expect(node.dataSource.afm.filters[0]).toEqual(visFilters[0]);
+        }, done);
     });
 
     it('should add date filter, if it has different id', (done) => {
@@ -143,14 +134,11 @@ describe('Visualization', () => {
             />
         );
 
-        postpone(
-            () => {
-                const node: any = wrapper.getNode();
-                expect(node.dataSource.afm.filters).toHaveLength(2);
-                expect(node.dataSource.afm.filters[1]).toEqual(visFilters[0]);
-            },
-            done()
-        );
+        postpone(() => {
+            const node: any = wrapper.getNode();
+            expect(node.dataSource.afm.filters).toHaveLength(2);
+            expect(node.dataSource.afm.filters[1]).toEqual(visFilters[0]);
+        }, done);
     });
 
     it('should add attribute filter', (done) => {
@@ -170,14 +158,11 @@ describe('Visualization', () => {
             />
         );
 
-        postpone(
-            () => {
-                const node: any = wrapper.getNode();
-                expect(node.dataSource.afm.filters).toHaveLength(2);
-                expect(node.dataSource.afm.filters[0]).toEqual(visFilters[0]);
-            },
-            done()
-        );
+        postpone(() => {
+            const node: any = wrapper.getNode();
+            expect(node.dataSource.afm.filters).toHaveLength(2);
+            expect(node.dataSource.afm.filters[0]).toEqual(visFilters[0]);
+        }, done);
     });
 
     it('should handle slow requests', (done) => {
@@ -192,13 +177,9 @@ describe('Visualization', () => {
 
         wrapper.setProps({ identifier: 'table' });
 
-        postpone(
-            () => {
-                expect(wrapper.find(Table).length).toBe(1);
-            },
-            done(),
-            300
-        );
+        postpone(() => {
+            expect(wrapper.find(Table).length).toBe(1);
+        }, done, 300);
     });
 
     it('should not re-render with same props', (done) => {
@@ -217,15 +198,11 @@ describe('Visualization', () => {
             filters: []
         });
 
-        postpone(
-            () => {
-                // initial render without datasources & with created datasources
-                expect(spy).toHaveBeenCalledTimes(2);
-                spy.mockRestore();
-            },
-            done(),
-            300
-        );
+        postpone(() => {
+            // initial render without datasources & with created datasources
+            expect(spy).toHaveBeenCalledTimes(2);
+            spy.mockRestore();
+        }, done, 300);
     });
 
     it('should handle set state on unmounted component', (done) => {
