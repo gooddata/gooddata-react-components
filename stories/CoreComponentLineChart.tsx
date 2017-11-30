1ad9cbd5ad2e7a4b370ba32a3a4f5c25b7f6c398
Pavel Langhammer <pavel.langhammer@gooddata.com>
2017-11-30 09:22:35 +0100

    fixup! add grizzly

---
+++ stories/CoreComponentLineChart.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { LineChart } from '../src/components/core/LineChart';
+import { VisualizationTypes } from '../src/constants/visualizationTypes';
+import { CUSTOM_COLORS } from './data/colors';
+import { getResultWithTwoMeasuresAndOneAttribute } from '../src/execution/fixtures/SimpleExecutor.fixtures';
+import { getMdResultWithTwoMeasuresAndOneAttribute } from './data/metadataResult';
+import { DataSourceMock, MetadataSourceMock, onErrorHandler } from './mocks';
+import '../styles/scss/charts.scss';
+import { screenshotWrap } from './utils/wrap';
+
+storiesOf('LineChart', module)
+    .add('two measures, one attribute', () => (
+        screenshotWrap(
+            <div style={{ width: 800, height: 400 }}>
+                <LineChart
+                    dataSource={new DataSourceMock(getResultWithTwoMeasuresAndOneAttribute())}
+                    metadataSource={
+                        new MetadataSourceMock(getMdResultWithTwoMeasuresAndOneAttribute(VisualizationTypes.LINE))
+                    }
+                    onError={onErrorHandler}
+                />
+            </div>
+        )
+    ))
+    .add('custom colors', () => (
+        screenshotWrap(
+            <div style={{ width: 800, height: 400 }}>
+                <LineChart
+                    dataSource={new DataSourceMock(getResultWithTwoMeasuresAndOneAttribute())}
+                    metadataSource={
+                        new MetadataSourceMock(getMdResultWithTwoMeasuresAndOneAttribute(VisualizationTypes.LINE))
+                    }
+                    onError={onErrorHandler}
+                    config={{ colors: CUSTOM_COLORS }}
+                />
+            </div>
+        )
+    ));
