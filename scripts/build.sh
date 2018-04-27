ae4496999f7ca92241e0e5b4ec7dea3b558008eb
Pavel Zbytovsky <pavel.zbytovsky@gooddata.com>
2018-04-27 11:03:39 +0200

    build-script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
 
-build-css: node-sass --importer node_modules/node-sass-magic-importer/dist/cli.js -o styles/css styles/scss/main.scss
+build(){
+    rm -rf dist
+    tsc
+    cp -rf src/translations/ dist/translations/
+    node-sass --importer node_modules/node-sass-magic-importer/dist/cli.js -o styles/css styles/scss/main.scss
+}
+
+build-dev(){
+    rm -rf dist
+    tsc -p tsconfig.dev.json
+    cp -rf src/translations/ dist/translations/
+    node-sass --importer node_modules/node-sass-magic-importer/dist/cli.js -o styles/css styles/scss/main.scss
+}
+
+build-dev-watch(){
+    rm -rf dist
+    cp -rf src/translations/ dist/translations/
+    node-sass --importer node_modules/node-sass-magic-importer/dist/cli.js -o styles/css styles/scss/main.scss
+
+    tsc --watch -p tsconfig.dev.json
+}
 
 
+FLAG=$1
 
-
-rm -rf dist && tsc && cp -rf src/translations/ dist/translations/ && yarn build-css
-
-rm -rf dist && tsc -p tsconfig.dev.json && cp -rf src/translations/ dist/translations/ && yarn build-css
-
-watch:
-
-rm -rf dist && tsc --watch -p tsconfig.dev.json && cp -rf src/translations/ dist/translations/
+if [ "$FLAG" = "--dev" ]; then
+    build-dev
+elif [ "$FLAG" = "--dev-watch" ]; then
+    build-dev-watch
+else
+    build
+fi
