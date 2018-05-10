213835bd52010d457d6729e617cca3f3bc212c9f
David Ocetnik <david.ocetnik@gooddata.com>
2018-05-10 15:04:05 +0200

    RELATED: FET-97 Remove borders from stories screenshots

--- stories/utils/wrap.tsx
+++ stories/utils/wrap.tsx
@@ -13,7 +13,7 @@ export function wrap(
     const keyProp: any = key ? { key } : {};
     return (
         <IntlWrapper {...keyProp}>
-            <div style={{ height, width, minHeight, minWidth, border: '1px solid pink' }}>
+            <div style={{ height, width, minHeight, minWidth }}>
                 {component}
             </div>
         </IntlWrapper>
