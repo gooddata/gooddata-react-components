3cbf39de9e858bfade5969a31f4ae8ce529434bf
David Ocetnik <david.ocetnik@gooddata.com>
2018-10-10 14:35:14 +0200

    BUGFIX: RAIL-1161 AFMTable component uses empty itemIdentifiers array in resultSpec measure dimension when AFM doesn't contain measures

--- src/components/afm/tests/Table.spec.tsx
+++ src/components/afm/tests/Table.spec.tsx
@@ -5,10 +5,10 @@ import { testUtils } from '@gooddata/js-utils';
 import { Table } from '../Table';
 import { SortableTable } from '../../core/SortableTable';
 import { dummyExecuteAfmAdapterFactory } from './utils/DummyExecuteAfmAdapter';
-import { executionRequest } from './utils/dummyFixture';
+import { executionRequest, executionRequestWithoutMeasureAndWithoutResultSpec } from './utils/dummyFixture';
 
 describe('Table', () => {
-    it('should provide default resultSpec to the core Table with attributes', () => {
+    it('should provide default resultSpec to the core Table with attribute and measure', () => {
         const wrapper = mount((
             <Table
                 projectId="prId"
@@ -24,6 +24,26 @@ describe('Table', () => {
             expect(dimensions).toEqual([
                 { itemIdentifiers: ['departmentAttribute'] },
                 { itemIdentifiers: ['measureGroup'] }
+            ]);
+        });
+    });
+
+    it('should provide default resultSpec to the core Table with no measure (with only attributes)', () => {
+        const wrapper = mount((
+            <Table
+                projectId="projectId"
+                afm={executionRequestWithoutMeasureAndWithoutResultSpec.execution.afm}
+                adapterFactory={dummyExecuteAfmAdapterFactory}
+            />
+        ));
+
+        return testUtils.delay().then(() => {
+            wrapper.update();
+            const dimensions = wrapper.find(SortableTable).props().resultSpec.dimensions;
+
+            expect(dimensions).toEqual([
+                { itemIdentifiers: ['departmentAttribute'] },
+                { itemIdentifiers: [] }
             ]);
         });
     });
