1797f49328246beeaf4f76b4b0ce5ee6915494cc
Petr Janů <petr.janu@gooddata.com>
2019-09-19 16:20:15 +0200

    Upgrade back react-window to original version

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -23,3 +23,18 @@ declare module "react-day-picker/moment" {
     const parseDate: any;
     export { formatDate, parseDate };
 }
+
+// Mock some React types since we are supporting react@^16.5.2, but we use types for 16.0 due to inability to upgrade it
+// tslint:disable-next-line:no-namespace
+declare namespace React {
+    type FC<P = {}> = FunctionComponent<P>;
+
+    // tslint:disable-next-line:interface-name
+    interface FunctionComponent<P = {}> {
+        (props: any, context?: any): any;
+        propTypes?: any;
+        contextTypes?: any;
+        defaultProps?: any;
+        displayName?: string;
+    }
+}
