0c2563ab68a44a85643e110a9c4e99fa865d2e3e
Petr Janů <petr.janu@gooddata.com>
2019-09-27 16:20:17 +0200

    Move some services back

--- src/components/filters/DateFilter/index.ts
+++ src/components/filters/DateFilter/index.ts
@@ -1,44 +1,19 @@
 // (C) 2007-2019 GoodData Corporation
 import { granularityIntlCodes } from "./constants/i18n";
-import { defaultDateFilterOptions, defaultDateFilterProjectConfig } from "./constants/config";
+import { defaultDateFilterOptions } from "./constants/config";
 import { validateFilterOption } from "./validation/OptionValidation";
-import { mapDateFilterOptionToDateFilterValue } from "./utils/ValueConversions";
-import { convertDateFilterConfigToDateFilterOptions } from "./utils/ConfigConversions";
-import { removeEmptyKeysFromDateFilterOptions } from "./utils/GroupUtils";
-import { mergeProjectConfigWithDashboardConfig } from "./utils/ConfigMerging";
 import * as Translators from "./utils/Translations/Translators";
-import {
-    mapAllTimeFilterToAfm,
-    mapAbsoluteFilterToAfm,
-    mapRelativeFilterToAfm,
-} from "./utils/AFMConversions";
 import { applyExcludeCurrentPeriod, canExcludeCurrentPeriod } from "./utils/PeriodExlusion";
 import { getDateFilterTitle, getDateFilterRepresentation } from "./utils/Translations/DateFilterTitle";
-import {
-    validateDateFilterConfig,
-    getDateFilterConfigValidationMessages,
-    isSelectedOptionValid,
-} from "./utils/ConfigValidation";
 import { DateFilter, IDateFilterCallbackProps, IDateFilterStateProps, IDateFilterProps } from "./DateFilter";
 
 const DateFilterHelpers = {
     validateFilterOption,
-    mapDateFilterOptionToDateFilterValue,
-    convertDateFilterConfigToDateFilterOptions,
-    removeEmptyKeysFromDateFilterOptions,
-    mapAllTimeFilterToAfm,
-    mapAbsoluteFilterToAfm,
-    mapRelativeFilterToAfm,
     getDateFilterTitle,
     getDateFilterRepresentation,
-    validateDateFilterConfig,
-    getDateFilterConfigValidationMessages,
-    mergeProjectConfigWithDashboardConfig,
-    isSelectedOptionValid,
     granularityIntlCodes,
     applyExcludeCurrentPeriod,
     defaultDateFilterOptions,
-    defaultDateFilterProjectConfig,
     canExcludeCurrentPeriod,
 };
 
