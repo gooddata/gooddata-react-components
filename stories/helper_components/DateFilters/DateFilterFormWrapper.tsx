37bf88232cc182b854ed46afd8bb0e884481869e
Petr Janů <petr.janu@gooddata.com>
2019-10-08 10:26:07 +0200

    test 2

--- stories/helper_components/DateFilters/DateFilterFormWrapper.tsx
+++
@@ -1,8 +0,0 @@
-// (C) 2007-2019 GoodData Corporation
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-import { DateFilterFormWrapper } from "../../../src/components/filters/DateFilter/DateFilterFormWrapper/DateFilterFormWrapper";
-
-storiesOf("Helper components/DateFilter/DateFilterFormWrapper", module).add("renders", () => {
-    return <DateFilterFormWrapper isMobile={false}>Content</DateFilterFormWrapper>;
-});
