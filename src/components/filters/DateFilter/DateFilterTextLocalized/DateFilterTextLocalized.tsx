298cfc51cf704f2edb56b1a91666b3080268caac
Petr Janů <petr.janu@gooddata.com>
2019-10-29 16:27:25 +0100

    RELATED: ONE-4000 DateFilter helpers use SDK translations

--- src/components/filters/DateFilter/DateFilterTextLocalized/DateFilterTextLocalized.tsx
+++ src/components/filters/DateFilter/DateFilterTextLocalized/DateFilterTextLocalized.tsx
@@ -1,7 +1,7 @@
 // (C) 2019 GoodData Corporation
 import * as React from "react";
 import { injectIntl, InjectedIntlProps } from "react-intl";
-import { ExtendedDateFilters } from "@gooddata/typings";
+import { ExtendedDateFilters, Localization } from "@gooddata/typings";
 import { getDateFilterTitle } from "../utils/Translations/DateFilterTitle";
 
 interface IDateFilterTextLocalizedProps {
@@ -13,7 +13,7 @@ const DateFilterTextLocalizedComponent: React.FC<IDateFilterTextLocalizedProps &
     filter,
     excludeCurrentPeriod,
     intl,
-}) => <>{getDateFilterTitle(filter, excludeCurrentPeriod, intl)}</>;
+}) => <>{getDateFilterTitle(filter, excludeCurrentPeriod, intl.locale as Localization.ILocale)}</>;
 
 export const DateFilterTextLocalized = injectIntl<IDateFilterTextLocalizedProps>(
     DateFilterTextLocalizedComponent,
--- src/components/filters/DateFilter/index.ts
+++ src/components/filters/DateFilter/index.ts
@@ -2,15 +2,19 @@
 import { granularityIntlCodes } from "./constants/i18n";
 import { defaultDateFilterOptions } from "./constants/config";
 import { validateFilterOption } from "./validation/OptionValidation";
-import * as Translators from "./utils/Translations/Translators";
 import { mapOptionToAfm } from "./utils/AFMConversions";
 import { applyExcludeCurrentPeriod, canExcludeCurrentPeriod } from "./utils/PeriodExlusion";
-import { getDateFilterTitle, getDateFilterRepresentation } from "./utils/Translations/DateFilterTitle";
+import {
+    getDateFilterTitle,
+    getDateFilterRepresentation,
+    getDateFilterTitleUsingTranslator,
+} from "./utils/Translations/DateFilterTitle";
 import { DateFilter, IDateFilterCallbackProps, IDateFilterStateProps, IDateFilterProps } from "./DateFilter";
 
 const DateFilterHelpers = {
     validateFilterOption,
     getDateFilterTitle,
+    getDateFilterTitleUsingTranslator,
     getDateFilterRepresentation,
     granularityIntlCodes,
     applyExcludeCurrentPeriod,
@@ -20,11 +24,4 @@ const DateFilterHelpers = {
 };
 
 // This is 1:1 reexported by root index.ts and is part of SDK's public API
-export {
-    DateFilter,
-    IDateFilterCallbackProps,
-    IDateFilterStateProps,
-    IDateFilterProps,
-    DateFilterHelpers,
-    Translators as DateFilterTranslators,
-};
+export { DateFilter, IDateFilterCallbackProps, IDateFilterStateProps, IDateFilterProps, DateFilterHelpers };
