cb8a8b44747e2e6525e33794ed0c069a19a8252d
naibinh <binh.nguyen@gooddata.com>
2020-02-19 16:13:33 +0700

    FEATURE: SD-820 prevent component update with shouldComponentUpdate

--- src/components/core/base/test/GeoValidatorHOC.spec.tsx
+++ src/components/core/base/test/GeoValidatorHOC.spec.tsx
@@ -5,10 +5,12 @@ import { mount, ReactWrapper } from "enzyme";
 import { geoValidatorHOC } from "../GeoValidatorHOC";
 import { ErrorComponent } from "../../../simple/ErrorComponent";
 import { LOCATION_ITEM, SIZE_ITEM } from "../../../../helpers/tests/geoChart/fixtures";
+import { OnError } from "../../../../interfaces/Events";
 import { IGeoConfig } from "../../../../interfaces/GeoChart";
 
 interface ITestInnerComponentProps {
     config?: IGeoConfig;
+    onError?: OnError;
 }
 class TestInnerComponent extends React.Component<ITestInnerComponentProps> {
     public render() {
@@ -30,8 +32,9 @@ describe("GeoValidatorHOC", () => {
         return mount(<WrappedComponent {...props} />);
     };
 
-    it("should show GEO_LOCATION_MISSING error", async () => {
+    it("should show GEO_LOCATION_MISSING error", () => {
         const buckets: VisualizationObject.IBucket[] = [SIZE_ITEM];
+        const onError = jest.fn();
         const wrapper = createComponent({
             config: {
                 mapboxToken: "",
@@ -40,13 +43,16 @@ describe("GeoValidatorHOC", () => {
                     visualizationClass,
                 },
             },
+            onError,
         });
 
         expect(wrapper.find(ErrorComponent).exists()).toEqual(true);
+        expect(onError).toBeCalledTimes(1);
     });
 
-    it("should not show GEO_LOCATION_MISSING error", async () => {
+    it("should not show GEO_LOCATION_MISSING error", () => {
         const buckets: VisualizationObject.IBucket[] = [LOCATION_ITEM, SIZE_ITEM];
+        const onError = jest.fn();
         const wrapper = createComponent({
             config: {
                 mapboxToken: "",
@@ -55,8 +61,41 @@ describe("GeoValidatorHOC", () => {
                     visualizationClass,
                 },
             },
+            onError,
         });
 
         expect(wrapper.find(ErrorComponent).exists()).toEqual(false);
+        expect(onError).toBeCalledTimes(0);
+    });
+
+    it("should toggle ErrorComponent", () => {
+        const buckets: VisualizationObject.IBucket[] = [SIZE_ITEM];
+        const bucketsWithLocation: VisualizationObject.IBucket[] = [LOCATION_ITEM, SIZE_ITEM];
+        const onError = jest.fn();
+        const config: IGeoConfig = {
+            mapboxToken: "",
+            mdObject: {
+                buckets,
+                visualizationClass,
+            },
+        };
+        const configWithLocation: IGeoConfig = {
+            mapboxToken: "",
+            mdObject: {
+                buckets: bucketsWithLocation,
+                visualizationClass,
+            },
+        };
+        const wrapper = createComponent({ config, onError });
+        expect(wrapper.find(ErrorComponent).exists()).toEqual(true);
+        expect(onError).toBeCalledTimes(1);
+
+        wrapper.setProps({ config: configWithLocation, onError });
+        expect(wrapper.find(ErrorComponent).exists()).toEqual(false);
+        expect(onError).toBeCalledTimes(1);
+
+        wrapper.setProps({ config, onError });
+        expect(wrapper.find(ErrorComponent).exists()).toEqual(true);
+        expect(onError).toBeCalledTimes(2);
     });
 });
