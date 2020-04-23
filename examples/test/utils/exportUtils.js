aea406bb0ab708b12ea9392eacee0291037f835b
Minh Luu <minh.luu@gooddata.com>
2020-04-23 16:03:37 +0700

    TEST: QA-10123 Automation test cover export insight in UI SDK to XLSX, CSV

---
+++ examples/test/utils/exportUtils.js
@@ -0,0 +1,48 @@
+// (C) 2020 GoodData Corporation
+import path from "path";
+import { t as testController } from "testcafe";
+import fs from "fs";
+
+export const DOWNLOAD_PATH = "downloads";
+export const FileType = {
+    EXCEL: 1,
+    CSV: 2,
+    properties: {
+        1: { name: "Excel", code: "xlsx" },
+        2: { name: "CSV", code: "csv" },
+    },
+};
+
+export async function enableDownloadForHeadlessChrome(t) {
+    const { browserConnection } = t.testRun;
+    const { client } = browserConnection.provider.plugin.openedBrowsers[browserConnection.id];
+    const { Page } = client;
+
+    await Promise.all([Page.enable()]);
+
+    await Page.setDownloadBehavior({
+        behavior: "allow",
+        downloadPath: path.resolve(DOWNLOAD_PATH),
+    });
+}
+
+export const exportTo = async (type, title) => {
+    // max length of title is 50
+    const downloadPath = `${DOWNLOAD_PATH}/${title.substring(0, 50)}.${FileType.properties[type].code}`;
+    /* eslint-disable no-await-in-loop */
+    // Wait 60*500 ms or less
+    for (let i = 0; i < 120; i += 1) {
+        if (fs.existsSync(downloadPath)) break;
+        await testController.wait(500);
+    }
+    /* eslint-enable no-await-in-loop */
+    return downloadPath;
+};
+
+export const exportToExcel = async title => {
+    return exportTo(FileType.EXCEL, title);
+};
+
+export const exportToCSV = async title => {
+    return exportTo(FileType.CSV, title);
+};
