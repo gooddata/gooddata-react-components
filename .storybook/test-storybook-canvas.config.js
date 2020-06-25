bca1c7755d82e7d2d1c958a0003a295f4a6e55b9
De Do <de.do@gooddata.com>
2020-06-25 14:35:59 +0700

    BUGFIX: test misMatchThreshold: 0.1 for BackStopJS

--- .storybook/test-storybook-canvas.config.js
+++ .storybook/test-storybook-canvas.config.js
@@ -3,7 +3,7 @@ const DEFAULT_SCENARIO_CONFIG = {
     // Sometimes, Color/Position of labels in Mapbox is rendered incorrectly.
     // BackstopJS would wait for 200 milliseconds before taking a screenshot
     delay: 200,
-    misMatchThreshold: 0.3
+    misMatchThreshold: 0.1
 };
 
 const chartStories = [
